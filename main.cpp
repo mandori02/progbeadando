3e410ff477591afb8e94f0cddaaae8d99539ea52
mandori02 <kubalazs02@gmail.com>
2024-05-19 21:33:54 +0200

    Allaskoveto matrix letrehozva

--- main.cpp
+++ main.cpp
@@ -6,44 +6,45 @@
 using namespace std;
 using namespace genv;
 
-struct Tile{
-
-    int x,y,sx,sy;
-    int isMarked = -1;
-
-};
-
-struct Board{
-
-    int noszlop = 15;
-    int nsor = 15;
-    vector<vector<Tile>> allas;
-
-};
 
 void gridDrawer(){
 
     for (int i = 0; i <= 15; ++i) {
         int x = i * 40;
-        gout << move_to(x, 0) << line(0, 600);
+        gout << color(255,255,255) << move_to(x, 0) << line(0, 600);
     }
 
     for (int i = 0; i <= 15; ++i) {
         int y = i * 40;
-        gout << move_to(0, y) << line(600, 0);
+        gout << move_to(0, y) << color(255,255,255) << line(600, 0);
     }
 
-    gout << refresh;
+}
 
+void matrixToConsole(vector<vector<int>> m)
+{
+        for (int i = 0; i < 15; ++i) {
+        for (int j = 0; j < 15; ++j) {
+            cout << m[i][j] << "\t";
+        }
+        cout << endl;
+    }
 }
 
 int main()
 {
     gout.open(600,600);
     event ev;
+    vector<vector<int>> allas(15, vector<int>(15, 0));
+    matrixToConsole(allas);
+
+
+
+
     while (gin >> ev && ev.keycode != key_escape) {
         gridDrawer();
 
+        gout << refresh;
     }
     return 0;
 }
